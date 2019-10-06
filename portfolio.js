86cd9d8c81be715b04bced608c13c812b8c10438
Kiylah O'Brien <kiylah2000@gmail.com>
2019-10-06 15:08:35 -0400

    continued styling the rest of the menu bar, added a background image, started writting the javascript

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,24 @@
+$(document).ready(function() {
+    $(".portfolio").each(index, portfolio => {
+        const menu = $(portfolio).find(".menu");
+        const menuAnchors = $(menu).find("a");
+        const content = $(portfolio).find(".content");
 
+        menuAnchors.each((i, anchor) => {
+            $(anchor).on("click", e => {
+                e.preventDefault();
+                $(menuAnchors).removeClass("active");
+                $(anchor).addClass("active");
+                let targetElement = $(anchor).attr("href");
+                $(content).removeClass("active");
+                $(".content" + targetElement).addClass("active");
+            });
+        });
+    });
+
+    $(window).on("load resize", function() {
+        if (window.matchesMedia("min-width:768px").matches) {
+            $(".content-inner")
+        }
+    });
+});
